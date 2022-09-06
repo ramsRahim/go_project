8abf5b6152689bd44df601fe76f3160c1f5650e5
Rahim Hossain <rahimhossain23@gmail.com>
2022-09-06 20:21:12 +0600

    unit test improved

--- LexiconSearch_test.go
+++ LexiconSearch_test.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"testing"
 )
+
+func writeDummyLexicon(t *testing.T) string {
+	t.Helper()
+
+	dummy := make(map[string]string)
+	dummy["COME"] = "k V m"
+	dummy["WORDS"] = "w 3` d z"
+	dummy["MECCA"] = "m E k @"
+
+	str := ""
+	for k, v := range dummy {
+		str += fmt.Sprintf("%s\t%s\n", k, v)
+	}
+	lexPath := path.Join(t.TempDir(), "lexicon.txt")
+	err := os.WriteFile(lexPath, []byte(str), 0644)
+
+	if err != nil {
+		t.Fatalf("unexpected error when writing lexicon file, err=%v", err)
+	}
+
+	return lexPath
+}
 
 func TestLoadLexicon(t *testing.T) {
 	tests := []struct {
@@ -15,24 +38,10 @@ func TestLoadLexicon(t *testing.T) {
 		{"WORDS", "w 3` d z"},
 		{"MECCA", "m E k @"},
 	}
-	dummy := make(map[string]string)
-	dummy["COME"] = "k V m"
-	dummy["WORDS"] = "w 3` d z"
-	dummy["MECCA"] = "m E k @"
 
-	str := ""
-	for k, v := range dummy {
-		str += k + "\t" + v + "\n"
-	}
-	lex_path := path.Join(t.TempDir(), "lexicon.txt")
-	err := os.WriteFile(lex_path, []byte(str), 0644)
-
-	if err != nil {
-		t.Fatalf("unexpected error when writing lexicon file, err=%v", err)
-	}
-
+	lexPath := writeDummyLexicon(t)
 	for _, tc := range tests {
-		if got, _ := LoadLexicon(lex_path); got[tc.n] != tc.want {
+		if got, _ := LoadLexicon(lexPath); got[tc.n] != tc.want {
 			t.Errorf("got %s, want %s", got[tc.n], tc.want)
 		}
 	}
